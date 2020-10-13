2e7cc214c507b1d0fad636fce458332fa64b3331
Anti Mono <antimono333@protonmail.com>
2020-10-13 14:17:24 +0100

    Analytics fix

--- pages/_app.js
+++ pages/_app.js
@@ -8,56 +8,53 @@ import { RecoilRoot } from "recoil"
 import SurveyForm from "../components/surveyForm"
 import { modalState } from "../utils/atoms"
 import Head from "next/head"
+import PlausibleProvider from "next-plausible"
 
 function MyApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
     <ApolloProvider client={apolloClient}>
-      <RecoilRoot>
-        <ThemeProvider theme={theme}>
-          <Head>
-            <link
-              rel="icon"
-              type="image/png"
-              href="https://shitscared.dev/apple-touch-icon-152x152.png"
+      <PlausibleProvider domain="shitscared.dev">
+        <RecoilRoot>
+          <ThemeProvider theme={theme}>
+            <Head>
+              <link
+                rel="icon"
+                type="image/png"
+                href="https://shitscared.dev/apple-touch-icon-152x152.png"
+              />
+            </Head>
+            <DefaultSeo
+              title="Shit Scared Developers"
+              description="Hello app developers. Post anonymously about your experiences with the App Store and Google Play."
+              noindex={(process.env.ENV != "production" && false) || true}
+              canonical="https://shitscared.dev"
+              openGraph={{
+                type: "website",
+                locale: "en_IE",
+                url: "https://shitscared.dev",
+                site_name: "Shit Scared Developers",
+                images: [
+                  {
+                    url: "https://shitscared.dev/ogimage.png",
+                    width: 1200,
+                    height: 630,
+                    alt: "Shit Scared Developers from Apple and Google"
+                  }
+                ]
+              }}
+              twitter={{
+                handle: "@shitscareddev",
+                site: "@site",
+                cardType: "summary_large_image"
+              }}
             />
-            <script
-              async
-              defer
-              data-domain="shitscared.dev"
-              src="https://plausible.io/js/plausible.js"
-            ></script>
-          </Head>
-          <DefaultSeo
-            title="Shit Scared Developers"
-            description="Hello app developers. Post anonymously about your experiences with the App Store and Google Play."
-            noindex={(process.env.ENV != "production" && false) || true}
-            canonical="https://shitscared.dev"
-            openGraph={{
-              type: "website",
-              locale: "en_IE",
-              url: "https://shitscared.dev",
-              site_name: "Shit Scared Developers",
-              images: [
-                {
-                  url: "https://shitscared.dev/ogimage.png",
-                  width: 1200,
-                  height: 630,
-                  alt: "Shit Scared Developers from Apple and Google"
-                }
-              ]
-            }}
-            twitter={{
-              handle: "@shitscareddev",
-              site: "@site",
-              cardType: "summary_large_image"
-            }}
-          />
-          <SurveyForm />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </RecoilRoot>
+            <SurveyForm />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </RecoilRoot>
+      </PlausibleProvider>
     </ApolloProvider>
   )
 }

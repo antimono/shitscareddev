9ff1c71321b91ad397939c65352e099e47bf1210
Anti Mono <antimono333@protonmail.com>
2020-11-13 18:21:07 +0000

    Sitemap fix

--- utils/apolloClient.js
+++ utils/apolloClient.js
@@ -1,9 +1,17 @@
 import { withApollo } from "next-apollo"
 import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { GraphQLClient } from "graphql-request"
+
+const GRAPHQL_URL = "https://keen-snake-21.hasura.app/v1/graphql"
+
+const graphql_client = new GraphQLClient(GRAPHQL_URL, { headers: {} })
 
 const apolloClient = new ApolloClient({
-  uri: "https://keen-snake-21.hasura.app/v1/graphql",
+  uri: GRAPHQL_URL,
   cache: new InMemoryCache()
 })
+
+export const graphlqlFetch = (query, variabels) =>
+  graphql_client.request(query, variabels).then(data => data)
 
 export default withApollo(apolloClient)
